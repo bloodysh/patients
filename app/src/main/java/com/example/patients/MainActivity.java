ab34bc4ab63e2b98f0b779b1bf99fbab38495f91
benmsatef.anas <anas.benmsatef@lycee-faure.fr>
2023-05-11 17:29:17 +0200

    session 2

--- app/src/main/java/com/example/patients/MainActivity.java
+++ app/src/main/java/com/example/patients/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.patients;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.patients.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Patient unPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.boutonValider.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                String nom= binding.SaisieNom.getText().toString();
+                String prenom= binding.SaisiePrenom.getText().toString();
+                float taille = Float.parseFloat(binding.SaisieTaille.getText().toString())/100;
+                int numChambre = Integer.valueOf(binding.SaisieChambre.getText().toString());
+                int poids = Integer.valueOf(binding.SaisiePoids.getText().toString());
+
+                unPatient= new Patient(nom, prenom, taille, numChambre, poids);
+
+                Toast.makeText(MainActivity.this, unPatient.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        public String afficherPatient(){
+            return nom, prenom, taille, numChambre, poids
+        }
+
     }
 }
--- app/src/main/java/com/example/patients/Patient.java
+++ app/src/main/java/com/example/patients/Patient.java
@@ -1,11 +1,91 @@
 package com.example.patients;
 
 public class Patient {
-    private String Nom;
-    private String Prenom;
-    private float Taille;
-    private int Poids;
-    private int NumChambre;
+    private String nom;
+    private String prenom;
+    private float taille;
+    private int poids;
+    private int numChambre;
 
-    public Patient{String Nom, String Prenom, float Taille, int Poids}
+    public Patient(String nom, String prenom, float taille, int poids, int numChambre) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.taille = taille;
+        this.poids = poids;
+        this.numChambre = numChambre;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public float getTaille() {
+        return taille;
+    }
+
+    public void setTaille(float taille) {
+        this.taille = taille;
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    public int getNumChambre() {
+        return numChambre;
+    }
+
+    public void setNumChambre(int numChambre) {
+        this.numChambre = numChambre;
+    }
+    public float calculerImc(){
+        float inc=0;
+        if(taille>0 && poids>0){
+            inc= Math.round(poids/(taille*taille));
+        }
+        return inc;
+    }
+    public String resultat (){
+        String body;
+        if (calculerImc() < 18.5){
+            body= " Vous etes maigres";
+        }
+        else if(calculerImc() >= 18.25 && calculerImc()<25){
+            body= " Vous êtes normales";
+        }
+        else if(calculerImc() >= 25 && calculerImc()<30){
+            body= " Vous êtes en surpoids";
+        }
+        else if(calculerImc() >= 30 && calculerImc()<35){
+            body= " Obesité modérée";
+        }
+        else if(calculerImc() >= 35 && calculerImc()<40){
+            body= " Obesité sévère";
+        }
+        else{
+            body= " Obesité massive";
+        }
+        return body;
+    }
+    public String toString(){
+        return nom +" "+ prenom + " votre IMC est :" + calculerImc() +"," + resultat();
+
+    }
 }
+
