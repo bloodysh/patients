630b2ebbcdb979a793e1b711e951f6238cd78d89
benmsatef.anas <anas.benmsatef@lycee-faure.fr>
2023-05-15 11:56:25 +0200

    session 3 Lundi

--- app/src/main/java/com/example/patients/MainActivity.java
+++ app/src/main/java/com/example/patients/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.example.patients.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private Patient unPatient;
+    Services monService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,36 @@ public class MainActivity extends AppCompatActivity {
                 float taille = Float.parseFloat(binding.SaisieTaille.getText().toString())/100;
                 int numChambre = Integer.valueOf(binding.SaisieChambre.getText().toString());
                 int poids = Integer.valueOf(binding.SaisiePoids.getText().toString());
+                String genrePatient = "";
+                if(binding.radioButton3.isChecked()){
+                    genrePatient="Masculin";
+                }
+                else{
+                    genrePatient="Feminin";
+                }
 
                 unPatient= new Patient(nom, prenom, taille, numChambre, poids);
+                monService.ajouterPatient(unPatient);
 
                 Toast.makeText(MainActivity.this, unPatient.afficherPatient() ,Toast.LENGTH_LONG).show();
             }
         });
 
+        monService = new Services("Service test");
+
+        ArrayAdapter<Patient> adapter = new ArrayAdapter<Patient>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, monService.getPatients());
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerAfficherPatient.setAdapter(adapter);
+
+
+
+        binding.boutonAfficherPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, unPatient.afficherPatient() ,Toast.LENGTH_LONG).show();
+            }
+
+        });
     };
 }
--- app/src/main/java/com/example/patients/Services.java
+++ app/src/main/java/com/example/patients/Services.java
@@ -1,4 +1,17 @@
 package com.example.patients;
+import java.util.ArrayList;
 
 public class Services {
+    private String nom;
+    private ArrayList<Patient> patients;
+
+    public Services(String nom){
+        this.nom= nom;
+        patients= new ArrayList<Patient>();
+    }
+
+    public void ajouterPatient(Patient patient) {patients.add(patient);}
+
+    public ArrayList<Patient> getPatients() {return patients;}
+
 }
